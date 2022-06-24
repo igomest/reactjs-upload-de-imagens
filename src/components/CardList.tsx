47fd820f2c2ddcf8ead782d64a380c3754bb484b
Ivan Gomes <ivanjunior.pt@gmail.com>
2022-06-24 10:17:21 -0300

    render card

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -17,17 +17,29 @@ interface CardsProps {
 
 export function CardList({ cards }: CardsProps): JSX.Element {
   // TODO MODAL USEDISCLOSURE
-
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [urlImg, setUrlImg] = useState('');
   // TODO SELECTED IMAGE URL STATE
 
   // TODO FUNCTION HANDLE VIEW IMAGE
+
+  function handleViewImage(url: string): void {
+    setUrlImg(url);
+    onOpen();
+  }
+
+  console.log(cards);
 
   return (
     <>
       {/* TODO CARD GRID */}
       <Grid templateColumns="repeat(3, 1fr)" gap="40px">
-        {cards.map(card => (
-          <Card key={card.id} data={card} viewImage={null} />
+        {cards?.map(data => (
+          <Card
+            key={data.id}
+            data={data}
+            viewImage={() => handleViewImage(data.url)}
+          />
         ))}
       </Grid>
 
